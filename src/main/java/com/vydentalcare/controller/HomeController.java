5333a35f8abac2a8f0c0ab4643ffa8ffb74615d7
Nishit <bhandvanishit@gmail.com>
2025-08-28 22:03:59 +0530

    pop up

--- src/main/java/com/vydentalcare/controller/HomeController.java
+++ src/main/java/com/vydentalcare/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.vydentalcare.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -20,8 +21,9 @@ public class HomeController {
     }
 
     @GetMapping("/appointment-success")
-    public String appointmentSuccess(Model model) {
+    public String appointmentSuccess(Model model,RedirectAttributes redirectAttributes) {
         model.addAttribute("message", "Appointment booked successfully!");
+        redirectAttributes.addFlashAttribute("message", "Data saved successfully!");
         return "index";
     }
 
